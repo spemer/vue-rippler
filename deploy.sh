569d2ee5508454cae29b35e0093c8d48b58f71ea
spemer <ghsspower@gmail.com>
2019-03-26 16:57:53 +0900

    Add files via upload

--- deploy.sh
+++ deploy.sh
@@ -2,36 +2,92 @@
 BOLD=$(tput bold)
 RESET=$(tput sgr0)
 
-echo "=============================="
+echo "============================================================"
 echo "${BOLD}${PWD##*/}${RESET}"
-echo "=============================="
+echo "============================================================"
 
-# docs dir
-cd docs
-
+#============================================================
 # serve with hot reload at localhost:8020
-npm run dev
+#============================================================
+npm_run_dev() {
+  while true; do
+    printf "\n"
+    read -p "${BOLD}npm run dev? (Y/n)${RESET}" yn
+    case ${yn} in
+      [Yy]* ) cd docs && npm run dev; break;;
+      [Nn]* ) return 0;;
+      * ) echo "Please answer yes or no.";;
+    esac
+  done
+}
 
+#============================================================
 # build for production with minification
-npm run build
+#============================================================
+npm_run_build() {
+  while true; do
+    printf "\n"
+    read -p "${BOLD}npm run build? (Y/n)${RESET}" yn
+    case ${yn} in
+      [Yy]* ) npm run build && cd -; break;;
+      [Nn]* ) return 0;;
+      * ) echo "Please answer yes or no.";;
+    esac
+  done
+}
 
-# root dir
-cd -
+#============================================================
+# git commit
+#============================================================
+git_commit() {
+  while true; do
+    printf "\n"
+    read -p "${BOLD}git commit? (Y/n)${RESET}" yn
+    case ${yn} in
+      [Yy]* )
+        IFS= read -r -p "${BOLD}Enter commit message: ${RESET}" commitmsg
 
-# get commit message
-printf "\n"
-IFS= read -r -p "${BOLD}Enter commit message: ${RESET}" commitmsg
+        # if commitmsg empty
+        if [ -z "$commitmsg" ]
+        then
+          echo "${BOLD}Commit message is empty${RESET}"
+          commitmsg="Add files via upload"
+        fi
 
-# if commitmsg empty
-if [ -z "$commitmsg" ]
-then
-    echo "${BOLD}Commit message is empty${RESET}"
-    commitmsg="Add files via upload"
-fi
+        printf "\n"
+        git add .
+        git commit -m "$commitmsg"
+        break;;
 
-printf "\n"
-git add .
-git commit -m "$commitmsg"
-git push
+      [Nn]* ) return 0;;
+      * ) echo "Please answer yes or no.";;
+    esac
+  done
+}
 
-exit
+#============================================================
+# git push
+#============================================================
+git_push() {
+  while true; do
+    printf "\n"
+    read -p "${BOLD}git push? (Y/n)${RESET}" yn
+    case ${yn} in
+      [Yy]* ) git push; break;;
+      [Nn]* ) return 0;;
+      * ) echo "Please answer yes or no.";;
+    esac
+  done
+}
+
+#============================================================
+# main
+#============================================================
+main() {
+  npm_run_dev
+  npm_run_build
+  git_commit
+  git_push
+}
+
+main
